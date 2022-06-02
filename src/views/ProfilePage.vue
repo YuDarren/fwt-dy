a29b044ae175b1bdefb412a58a3cfe36f8b40b46
Darren Yu <denyoyu6450@gmail.com>
2022-06-03 01:27:13 +0800

    pfPage v1

---
+++ src/views/ProfilePage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {};
+</script>
+<template>
+  <div class="pf_main_page">
+    <div class="main_area"></div>
+    <div class="side_area"></div>
+  </div>
+</template>
+<style>
+.pf_main_page {
+  display: flex;
+}
+</style>
