297eda2d12fe6e0a3ce38b0ffa5638ec09a098aa
Darren Yu <denyoyu6450@gmail.com>
2022-06-02 22:44:10 +0800

    revise app.vue . add new img

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,25 @@
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import ProfilePage from "@/views/ProfilePage.vue";
 
 export default {
   components: {
     Header,
     Footer,
+    ProfilePage,
   },
 };
 </script>
 
 <template>
   <Header />
-  <router-view />
-
+  <ProfilePage />
   <Footer />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 body {
   margin: 0;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
