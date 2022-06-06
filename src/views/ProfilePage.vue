9e0397f36c8262115627df15bf57c17bac93388f
Darren Yu <denyoyu6450@gmail.com>
2022-06-06 23:17:12 +0800

    profile v1.2

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -3,8 +3,23 @@ export default {};
 </script>
 <template>
   <div class="pf_main_page">
-    <div class="main_area"></div>
-    <div class="side_area"></div>
+    <div class="main_area">
+      <div class="user_block">
+        <div class="userbg_photo"></div>
+        <div class="user_info"></div>
+      </div>
+      <div class="btn_block"></div>
+      <div class="userProfile_block"></div>
+      <div class="projects_block"></div>
+      <div class="skills_block"></div>
+      <div class="exp_block"></div>
+      <div class="edu_block"></div>
+    </div>
+    <div class="side_area">
+      <div class="dash_block"></div>
+      <div class="vistors_block"></div>
+      <div class="courses_block"></div>
+    </div>
   </div>
 </template>
 <style>
