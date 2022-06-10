ea4be4572b1e6dd3478056f763d22e50be772223
Darren Yu <denyoyu6450@gmail.com>
2022-06-10 17:04:12 +0800

    profile-project finish

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,5 +1,8 @@
 <script>
-export default {};
+import { MoreFilled, Edit, Upload } from "@element-plus/icons-vue";
+export default {
+  components: { MoreFilled, Edit, Upload },
+};
 </script>
 <template>
   <div class="pf_main_page">
@@ -89,27 +92,41 @@ export default {};
       </div>
       <div class="projects_block">
         <div class="project_tit">
-          <h2>Projects</h2>
-          <span>3 of 12</span>
+          <h2 id="tit">Projects</h2>
+          <h2 id="tit_num">3 of 12</h2>
         </div>
         <div class="project_main">
           <div class="main">
-            <div class="main_photo"></div>
-            <div class="main_tit">Zara redesign concept</div>
+            <div class="main_photo">
+              <a href="#"
+                ><img src="../assets/project/project-01.jpg" alt=""
+              /></a>
+            </div>
+            <div class="main_tit"><p>Zara redesign concept</p></div>
             <div class="main_time">UX/UI design, 15.07.2019</div>
           </div>
           <div class="main">
-            <div class="main_photo"></div>
-            <div class="main_tit">SCTHON event brand identity</div>
+            <div class="main_photo">
+              <a href="#"
+                ><img src="../assets/project/project-02.jpg" alt=""
+              /></a>
+            </div>
+            <div class="main_tit"><p>SCTHON event brand identity</p></div>
             <div class="main_time">Graphic design, 03.31.2019</div>
           </div>
           <div class="main">
-            <div class="main_photo"></div>
-            <div class="main_tit">Drozd. Brand identity. 2016</div>
+            <div class="main_photo">
+              <a href="#"
+                ><img src="../assets/project/project-03.jpg" alt=""
+              /></a>
+            </div>
+            <div class="main_tit"><p>Drozd. Brand identity. 2016</p></div>
             <div class="main_time">Graphic design, 03.04.2016</div>
           </div>
         </div>
-        <div class="project_sa"><a href="#">Show all (12)</a></div>
+        <div class="project_sa">
+          <a href="#"> <h3>Show all (12)</h3></a>
+        </div>
       </div>
       <div class="skills_block"></div>
       <div class="exp_block"></div>
@@ -370,10 +387,68 @@ export default {};
         font-size: 14px;
       }
       .about_sm {
-        color: #0275b1;
         text-transform: uppercase;
         font-weight: 700;
         font-size: 12px;
+        h3 {
+          color: #0275b1;
+        }
+      }
+    }
+    .projects_block {
+      background: #ffffff;
+      border-radius: 4px;
+      padding-left: 30px;
+      padding-right: 30px;
+      padding-top: 5px;
+      padding-bottom: 10px;
+      margin-top: 20px;
+      font-family: Gotham;
+      .project_tit {
+        display: flex;
+        align-items: center;
+        font-size: 18px;
+
+        #tit {
+          color: #181818;
+          font-weight: bold;
+        }
+        #tit_num {
+          padding-left: 20px;
+          color: #747474;
+          font-weight: normal;
+        }
+      }
+      .project_main {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .main {
+          display: block;
+
+          .main_photo {
+            img {
+              width: 250px;
+              height: 160px;
+            }
+          }
+          .main_tit {
+            font-size: 16px;
+            font-weight: 700;
+          }
+          .main_time {
+            font-size: 10px;
+            font-weight: 100;
+          }
+        }
+      }
+      .project_sa {
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 12px;
+        h3 {
+          color: #0275b1;
+        }
       }
     }
   }
