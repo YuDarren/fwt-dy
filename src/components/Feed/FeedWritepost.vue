ac0e6ae6442f55b42cec034f016e2513aff694e4
Darren Yu <denyoyu6450@gmail.com>
2022-06-16 22:09:40 +0800

    feedpage sortlin v1.2

---
+++ src/components/Feed/FeedWritepost.vue
@@ -0,0 +1,116 @@
+<script>
+import { Paperclip, Picture, Film, Position } from "@element-plus/icons-vue";
+export default {
+  components: { Paperclip, Picture, Film, Position },
+};
+</script>
+<template>
+  <div class="writepost_block">
+    <div class="writepost">
+      <div class="post_title">new post</div>
+      <div class="write">
+        <div class="write_input">
+          <input type="text" placeholder="What’s on your mind?" />
+        </div>
+        <div class="btns">
+          <div:focus-within class="write_btns">
+            <a href="#">
+              <el-icon><Paperclip /></el-icon
+            ></a>
+          </div:focus-within>
+          <div:focus-within class="write_btns">
+            <a href="#">
+              <el-icon><Picture /></el-icon
+            ></a>
+          </div:focus-within>
+          <div:focus-within class="write_btns">
+            <a href="#"
+              ><el-icon><Film /></el-icon
+            ></a>
+          </div:focus-within>
+          <div:focus-within class="write_btns" autofocus>
+            <a href="#">
+              <el-icon><Position /></el-icon
+            ></a>
+          </div:focus-within>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.writepost_block {
+  background: #ffffff;
+  border-radius: 4px;
+  .post_title {
+    margin-left: 20px;
+    margin-right: 20px;
+    padding-bottom: 20px;
+    padding-top: 20px;
+    font-weight: 600;
+    text-transform: uppercase;
+    font-size: 12px;
+    border-bottom: 1px solid #f4f4f4;
+  }
+  .write {
+    margin-left: 20px;
+    margin-right: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .write_input {
+      input {
+        height: auto;
+        outline: none;
+        border: none;
+        width: 80%;
+        padding-bottom: 20px;
+        padding-top: 20px;
+        color: #181818;
+        opacity: 0.2;
+        font-size: 18px;
+      }
+    }
+    .btns {
+      width: 20%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .write_btns {
+        width: 32px;
+        height: 32px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        a {
+          color: #181818;
+          opacity: 0.2;
+          .el-icon {
+            width: 32px;
+            height: 32px;
+            font-size: 24px;
+          }
+        }
+      }
+      .write_btns:focus-within {
+        width: 32px;
+        height: 32px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: #0275b1;
+        border-radius: 4px;
+        a {
+          color: #ffffff;
+          opacity: 1;
+          .el-icon {
+            width: 32px;
+            height: 32px;
+            font-size: 24px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Feed.vue
@@ -0,0 +1,81 @@
+<script>
+import { ArrowDown } from "@element-plus/icons-vue";
+import FeedWritepost from "@/components/Feed/FeedWritepost.vue";
+export default {
+  components: { ArrowDown, FeedWritepost },
+};
+</script>
+<template>
+  <div class="feed_main_page">
+    <div class="fe_main_area">
+      <FeedWritepost />
+      <div class="sortline_block">
+        <div class="sortline">
+          <div class="line"></div>
+          <div class="text">
+            Sort by:
+            <a href="">
+              trending<el-icon><ArrowDown /></el-icon
+            ></a>
+          </div>
+          <div class="line"><span></span></div>
+        </div>
+      </div>
+      <div class="userpost_block"></div>
+      <div class="userpost_block"></div>
+      <div class="userpost_block"></div>
+      <div class="userpost_block"></div>
+    </div>
+    <div class="fe_side_area"></div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.feed_main_page {
+  display: flex;
+  padding-top: 40px;
+  margin: 0 auto;
+  width: calc(100% - 260px);
+  max-width: 1920px;
+  font-family: Gothan;
+  .fe_main_area {
+    margin-right: 20px;
+    width: 72%;
+    .sortline_block {
+      margin-top: 20px;
+      .sortline {
+        padding-top: 20px;
+        padding-bottom: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .line {
+          border: 1px solid #e7e7e7;
+        }
+        .text {
+          font-size: 12px;
+          text-transform: uppercase;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          a {
+            color: #0275b1;
+            display: flex;
+            align-items: center;
+            margin-left: 5px;
+            .el-icon {
+              margin-left: 5px;
+              color: #0275b1;
+              font-size: 12px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .fe_side_area {
+    margin-right: 20px;
+    width: 28%;
+  }
+}
+</style>
