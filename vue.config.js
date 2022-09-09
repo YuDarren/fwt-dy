7b02233a63ba0d4f97f813ccc2f97fc4944e52f9
Darren Yu <denyoyu6450@gmail.com>
2022-09-09 13:31:41 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  /*測試用 */
   publicPath: process.env.NODE_ENV === "production" ? "/linkedin/" : "/",
   transpileDependencies: true,
 });
